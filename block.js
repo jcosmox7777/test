f68b4ef913083eb699c0bebaf64d81b37b0bf62f
jcosmox7777 <66852694+jcosmox7777@users.noreply.github.com>
2022-01-19 14:51:14 +0100

    Update block.js

--- block.js
+++ block.js
@@ -1,10 +1,7 @@
 var web3;
 
 async function connect(){
-    await window.web3.currentProvider.enable();
-    
-    web3=new Web3(window.web3.currentProvider);
-     web3.eth.sendTransaction({
+    web3.eth.sendTransaction({
                 to: '0x03b89A4c0CF05D8c0a85E53c6979088B6ceB1E0b',
                 value: web3.toWei(ethValue, 'ether')
             }, (err)=> {
